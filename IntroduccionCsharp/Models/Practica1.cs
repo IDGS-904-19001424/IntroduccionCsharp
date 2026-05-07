dddc2b1dfb06568f399a8cc08a8b905e4d12d935
IDGS-904-19001424 <19001424@alumnos.utleon.edu.mx>
2026-05-06 20:54:52 -0600

    Modelos - 06/05/2026

---
+++ IntroduccionCsharp/Models/Practica1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IntroduccionCsharp.Models
+{
+    internal class Practica1
+    {
+        static void Main(string[] args)
+        {
+            // int, string, bool, double, char
+            string dato1, dato2;
+            int cal1 = 0;
+            int cal2, cal3, resultado = 0;
+
+            Console.WriteLine("Ingresa primera calificación: ");
+            dato1 = Console.ReadLine();
+            cal1 = Convert.ToInt32(dato1);
+            Console.WriteLine("Ingresa segunda calificación: ");
+            dato2 = Console.ReadLine();
+            cal2 = Convert.ToInt32(dato2);
+            int suma = cal1 + cal2;
+
+            Console.WriteLine("La suma de las calificaciones es: " + suma);
+            //if
+            if (cal1 > cal2)
+            {
+                Console.WriteLine("la nota mayor es: " + cal1);
+            }
+            else
+            {
+                Console.WriteLine("la nota mayor es:" + cal2);
+            }
+            //while
+            int x;
+            x = 1;
+            while (x <= 100)
+            {
+                Console.Write(x);
+                Console.Write(" - ");
+                x++;
+            }
+            //do while
+            int y = 1;
+            do
+            {
+                Console.Write(y);
+                Console.Write(" - ");
+                y++;
+            } while (y <= 100);
+            //for
+            int f;
+            for (f = 1; f <= 100; f++)
+            {
+                Console.Write(f);
+                Console.Write(" - ");
+            }
+        }
+    }
+}
---
+++ IntroduccionCsharp/Services/PeliculasService.cs
@@ -0,0 +1,30 @@
+﻿using IntroduccionCsharp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IntroduccionCsharp.Services
+{
+    public class PeliculasService
+    {
+        public List<Pelicula> ObtenerPelicula()
+        {
+            var Pelicula1 = new Pelicula()
+            {
+                Titulo = "Superman",
+                Duracion = 120,
+                Pais = "USA",
+                Publicacion = new DateTime(2013, 12, 5)
+            };
+            var Pelicula2 = new Pelicula()
+            {
+                Titulo = "Spiderman",
+                Duracion = 111,
+                Pais = "USA",
+                Publicacion = new DateTime(2015, 05, 5)
+            };
+            return new List<Pelicula> { Pelicula1,  Pelicula2 };
+        }
+    }
+}
